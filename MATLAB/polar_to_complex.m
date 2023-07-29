3f32236cd316bba71ba452068ed3dfb8cfdf32ad
jbw9964 <jbw9964@gmail.com>
2023-07-29 14:42:41 +0900

    delete unnecessary files

--- MATLAB/polar_to_complex.m
+++
@@ -1,8 +0,0 @@
-function result_array = polar_to_complex(array)
-    r = array(:, :, 1);
-    theta = array(:, :, 2);
-    real_part = r.*cos(theta);
-    imag_part = r.*sin(theta);
-    
-    result_array = complex(real_part, imag_part);
-end
--- matlabcode.m
+++
@@ -1,4 +0,0 @@
-
-% layer_to_remove = 'multiply_OutputLayer_PLACEHOLDER';
-% remove_layer = removeLayers(layer, layer_to_remove);
-% analyzeNetwork(remove_layer)
